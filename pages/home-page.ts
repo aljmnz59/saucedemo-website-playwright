66f38374373eefdd0a9a821acdf5a1ab073baf4a
aljunjimenez <aljunjimenez59@gmail.com>
2026-01-26 15:33:55 +0800

    first commit

---
+++ pages/home-page.ts
@@ -0,0 +1,60 @@
+import { Locator, expect, Page } from "@playwright/test";
+
+export class HomePage {
+    //variables
+    readonly page:Page;
+    readonly pageLogo:Locator;
+    readonly username:Locator;
+    readonly password:Locator;
+    readonly loginBtn:Locator;
+    readonly errorLoginContainer:Locator
+    readonly errorLoginMsg: string = `Epic sadface: Username and password do not match any user in this service`; 
+
+    //constructor
+    constructor (page: Page){
+        this.page = page;
+        this.pageLogo = page.locator('.login_logo');
+        this.username = page.getByPlaceholder('Username');
+        this.password = page.getByPlaceholder('Password');
+        this.loginBtn = page.getByRole('button', {name : 'Login'});
+        this.errorLoginContainer = page.locator('.error-message-container.error');
+    }
+
+    //methods
+    async assertPageUrl(pageUrl: RegExp){
+        await expect(this.page).toHaveURL(pageUrl)
+    }
+
+    async assertPageLogo(){
+        await expect(this.pageLogo).toBeVisible();
+        await expect(this.pageLogo).toHaveText('Swag Labs');
+    }
+
+    async clickUsername(){
+        await this.username.click();
+    }
+
+    async fillUsername(userName: string){
+        await this.username.fill(userName);
+    }
+
+    async clickPassword(){
+        await this.password.click();
+    }
+
+    async fillPassword(password: string){
+        await this.password.fill(password);
+    }
+
+    async clickLoginBtn(){
+        await this.loginBtn.click();
+    }
+
+    async assertErrorLoginMsg(){
+        await expect(this.errorLoginContainer).toHaveText(this.errorLoginMsg);
+    }
+
+
+}
+
+export default HomePage;
---
+++ tests/home.spec.ts
@@ -0,0 +1,44 @@
+import {test, expect, Page} from '@playwright/test';
+import { HomePage } from '../pages/home-page'; 
+
+const URL = 'https://www.saucedemo.com/';
+let homePage: HomePage;
+const pageUrl = /.*inventory.html/
+
+test.beforeEach(async ({page}) => {
+    await page.goto(URL);
+    homePage = new HomePage(page);
+});
+
+test('Check saucedemo logo', async () => {
+    await homePage.assertPageLogo();
+});
+
+test('Verify succesful login using valid credentials', async () => {
+    await homePage.clickUsername();
+    await homePage.fillUsername('standard_user');
+    await homePage.clickPassword();
+    await homePage.fillPassword('secret_sauce');
+    await homePage.clickLoginBtn();
+    await homePage.assertPageUrl(pageUrl);
+});
+
+test.describe('Verify unsuccessful login using invalid credentials', async () => {
+    test('Unsuccessful Login using invalid username', async ({page}) =>{
+    await homePage.clickUsername();
+    await homePage.fillUsername('sandard_user');
+    await homePage.clickPassword();
+    await homePage.fillPassword('secret_sauce');
+    await homePage.clickLoginBtn();
+    await homePage.assertErrorLoginMsg();
+});
+
+test('Unsuccessful Login using invalid password', async () => {
+    await homePage.clickUsername();
+    await homePage.fillUsername('error_user')
+    await homePage.clickPassword();
+    await homePage.fillPassword('sauce_secret');
+    await homePage.clickLoginBtn();
+    await homePage.assertErrorLoginMsg();
+});
+})
